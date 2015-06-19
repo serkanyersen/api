1cbea2dbae198aefb09e11dceda7620a680779ad
Serkan Yersen <serkanyersen@gmail.com>
2015-06-19 15:14:14 -0700

    Add versioning, and logging

--- index.js
+++ index.js
@@ -1,21 +1,39 @@
 #!/usr/bin/env node
-var restify = require('restify');
-var sass = require('node-sass');
-// var args = process.argv.slice(2);
+'use strict';
 
+var restify = require('restify');
+var Logger = require('bunyan');
+var sass = require('node-sass');
+var versioning = require('restify-url-semver');
+var log = new Logger.createLogger({
+    name: 'api',
+    serializers: {
+        req: Logger.stdSerializers.req
+    }
+});
 var server = restify.createServer({
   name: 'api',
-  version: '1.0.0'
+  log: log,
+  version: ['1.0.0']
+});
+
+// Allow versioning
+server.pre(versioning());
+
+// Logging
+server.pre(function (request, response, next) {
+    request.log.info({ req: request }, 'REQUEST');
+    next();
 });
 
 server.use(restify.acceptParser(server.acceptable));
+server.use(restify.gzipResponse());
 server.use(restify.queryParser());
 server.use(restify.jsonp());
 server.use(restify.CORS());
 server.use(restify.bodyParser());
 
-server.post('/sass', function (req, res, next) {
-
+server.post({ path: '/sass', version: '1.0.0' }, function (req, res, next) {
     var result = sass.renderSync({
       data: req.params.code
     });
