5fba23b3442e0ab961e523b67572d45c6f2b2f6f
Serkan Yersen <serkanyersen@gmail.com>
2015-06-23 08:32:29 +0000

    Add IP to listen to

--- src/server.js
+++ src/server.js
@@ -20,6 +20,6 @@ server.use(restify.jsonp());
 server.use(restify.CORS());
 server.use(restify.bodyParser());
 
-routes(server).listen(9090, function () {
+routes(server).listen(9090, '0.0.0.0', function () {
     console.log('%s listening at %s', server.name, server.url);
 });
